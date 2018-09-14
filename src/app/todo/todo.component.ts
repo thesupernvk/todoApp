0728fcd112e231389cd7fd2c241fce0eee476451
Navin Vijaykumar <38689367+thesupernvk@users.noreply.github.com>
2018-09-14 12:36:29 +0200

    initial commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  tasks: any[];
+  task : String = "";
+
+  constructor() {
+    this.tasks = [];
+  }
+
+  ngOnInit() { }
+
+  onBlurMethod() {
+    if(this.task.length>0){
+      this.tasks.push({name: this.task, completed : false});
+      this.task = "";
+    }
+  }
+
+  removeTask(task){
+    for(let i=0; i<this.tasks.length; i++){
+      if(this.tasks[i].name === task.name){
+        this.tasks.splice(i,1);
+      }
+    }
+  }
+
+}
